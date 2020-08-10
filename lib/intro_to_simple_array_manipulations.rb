d152639d99a5b49c946f488cf365245acee6c386
Mahdi Hawary <mahdihawary@gmail.com>
2020-08-10 02:51:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,9 +1,12 @@
+require 'pry'
 def using_concat(array_1,array_2)
   array_1.concat(array_2)
   
 end
 def using_insert(array,new_element)
   array.insert(3,new_element)
+  binding.pry
+  puts array
 end
 def using_uniq (array)
   array.uniq
