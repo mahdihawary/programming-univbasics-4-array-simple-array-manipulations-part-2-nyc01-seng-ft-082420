0630b80eee129d098ce346ca54a2d119b5a656c7
Mahdi Hawary <mahdihawary@gmail.com>
2020-08-10 02:47:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,4 +6,14 @@ def using_insert(array,new_element)
   array.insert(3,new_element)
 end
 def using_uniq (array)
-  
+  array.uniq
+end
+def using_flatten(array)
+  array.flatten
+end
+def using_delete(array,string)
+  array.delete(string)
+end
+def using_delete_at(array,index)
+  array.delete_at(index)
+end
